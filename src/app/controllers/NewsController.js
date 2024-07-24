f0281f1a4bb7e77a7cabc8b0f4234bcc3d269d80
Thinh <155166003+NHThinh123@users.noreply.github.com>
2024-07-24 10:41:58 +0700

    mvc

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,11 @@
+class NewsController {
+  // [GET] /news
+  index(req, res) {
+    res.render("news");
+  }
+
+  show(req, res) {
+    res.send("new detail");
+  }
+}
+module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
+const route = require("./routes");
 
 app.use(express.static(path.join(__dirname, "public")));
 //http logger
@@ -20,13 +21,9 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 console.log(path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// init routes
 
-app.get("/tin-tuc", (req, res) => {
-  res.render("news");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
